53eb6be36afce29bfbdd4323f5faf6c224b8cdcb
Marcus P da Silva <marcus@rigetti.com>
2019-03-22 15:28:41 -0700

    Add tests

--- forest/benchmarking/tests/test_direct_fidelity_estimation.py
+++ forest/benchmarking/tests/test_direct_fidelity_estimation.py
@@ -6,6 +6,7 @@ from pyquil.gates import *
 from pyquil.numpy_simulator import NumpyWavefunctionSimulator
 from pyquil.operator_estimation import _one_q_state_prep
 
+from numpy.testing import assert_almost_equal
 
 def test_exhaustive_state_dfe(benchmarker: BenchmarkConnection):
     texpt = exhaustive_state_dfe(program=Program(X(0), X(1)), qubits=[0, 1],
@@ -25,7 +26,7 @@ def test_exhaustive_dfe(benchmarker: BenchmarkConnection):
     assert len(texpt) == 7 ** 1 - 1
 
 
-def test_exhaustive_dfe_run(benchmarker: BenchmarkConnection):
+def test_exhaustive_process_dfe_run(benchmarker: BenchmarkConnection):
     wfnsim = NumpyWavefunctionSimulator(n_qubits=1)
     process = Program(Z(0))
     texpt = exhaustive_process_dfe(program=process, qubits=[0], benchmarker=benchmarker)
@@ -39,8 +40,21 @@ def test_exhaustive_dfe_run(benchmarker: BenchmarkConnection):
         expectation = wfnsim.reset().do_program(prog).expectation(setting.out_operator)
         assert expectation == 1.
 
+def test_exhaustive_state_dfe_run(benchmarker: BenchmarkConnection):
+    wfnsim = NumpyWavefunctionSimulator(n_qubits=1)
+    process = Program(X(0))
+    texpt = exhaustive_state_dfe(program=process, qubits=[0], benchmarker=benchmarker)
+    for setting in texpt:
+        setting = setting[0]
+        prog = Program()
+        for oneq_state in setting.in_state.states:
+            prog += _one_q_state_prep(oneq_state)
+        prog += process
 
-def test_monte_carlo_dfe(benchmarker: BenchmarkConnection):
+        expectation = wfnsim.reset().do_program(prog).expectation(setting.out_operator)
+        assert expectation == 1.
+
+def test_monte_carlo_process_dfe(benchmarker: BenchmarkConnection):
     process = Program(CNOT(0, 1))
     texpt = monte_carlo_process_dfe(program=process, qubits=[0, 1], n_terms=10,
                                     benchmarker=benchmarker)
@@ -55,7 +69,24 @@ def test_monte_carlo_dfe(benchmarker: BenchmarkConnection):
         prog += process
 
         expectation = wfnsim.reset().do_program(prog).expectation(setting.out_operator)
-        assert expectation == 1.
+        assert_almost_equal(expectation,1.,decimal=7)
+
+def test_monte_carlo_state_dfe(benchmarker: BenchmarkConnection):
+    process = Program(H(0), CNOT(0, 1))
+    texpt = monte_carlo_state_dfe(program=process, qubits=[0, 1], n_terms=10,
+                                    benchmarker=benchmarker)
+    assert len(texpt) == 10
+
+    wfnsim = NumpyWavefunctionSimulator(n_qubits=2)
+    for setting in texpt:
+        setting = setting[0]
+        prog = Program()
+        for oneq_state in setting.in_state.states:
+            prog += _one_q_state_prep(oneq_state)
+        prog += process
+
+        expectation = wfnsim.reset().do_program(prog).expectation(setting.out_operator)
+        assert_almost_equal(expectation,1.,decimal=7)
 
 def test_ratio_variance():
     # If our uncertainty is 0 in each parameter, the uncertainty in the ratio should also be 0.
