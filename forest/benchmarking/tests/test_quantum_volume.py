ee4df14efc3e714a32e1d3f45527c30325b1c62a
Kyle <kyle.gulshen@rigetti.com>
2019-03-25 14:46:26 -0400

    Made test less sensitive by decoupling extraction test.

--- forest/benchmarking/tests/test_quantum_volume.py
+++ forest/benchmarking/tests/test_quantum_volume.py
@@ -6,18 +6,23 @@ np.random.seed(1)
 
 
 def test_ideal_sim_heavy_probs(qvm):
-    qvm.qam.random_seed = 2
+    qvm.qam.random_seed = 1
     depths = [2, 3]
 
     # silence warning from too few circuits, since 100 circuits is too slow
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        outcomes = measure_quantum_volume(qvm, num_circuits=40, num_shots=50, qubits=[0, 1, 2])
+        outcomes = measure_quantum_volume(qvm, num_circuits=40, num_shots=50, qubits=[0, 1, 2],
+                                          stop_when_fail=False)
 
-    assert extract_quantum_volume_from_results(outcomes) == 8
     target_probs = [0.788765, 0.852895]
     probs = [outcomes[depth][0] for depth in depths]
     np.testing.assert_allclose(probs, target_probs, atol=.05)
+
+
+def test_extraction():
+    outcomes = {2: (.72, .68), 3: (.7, .67), 4: (.69, .66)}
+    assert extract_quantum_volume_from_results(outcomes) == 8
 
 
 def test_qv_df_generation():
