527aea685ec30697dcc6386c4df33e2d2fb71d41
Kyle <kyle.gulshen@rigetti.com>
2019-04-23 16:57:36 -0400

    Updated rpe tests.

--- forest/benchmarking/tests/test_robust_phase_estimation.py
+++ forest/benchmarking/tests/test_robust_phase_estimation.py
@@ -7,71 +7,76 @@ from pyquil.quil import Program
 from pyquil.quilbase import Measurement
 
 import forest.benchmarking.robust_phase_estimation as rpe
-from forest.benchmarking.robust_phase_estimation import _run_rpe_program, _make_prog_from_df
+from forest.benchmarking.stratified_experiment import StratifiedExperiment, Layer, \
+    acquire_stratified_data
 
 
-def test_single_depth(qvm):
+def test_expectations_at_depth(qvm):
     qvm.qam.random_seed = 5
-    expected_outcomes = [0., .5, 1.0, .5]
+    q = 0
+    qubits = (q, )
+    expected_outcomes = [1., 0, -1., 0]
     for depth in [0, 1, 2, 3, 4]:
+        prep, meas, settings = rpe.all_eigenvector_prep_meas_settings(qubits, I(q))
+        layers = [Layer(depth, [RZ(pi/2, q) for _ in range(depth)], tuple(settings), qubits)]
+        expt = StratifiedExperiment(tuple(layers), qubits)
+        result = acquire_stratified_data(qvm, [expt], num_shots=500, parallelize_layers=True)[0]
         for meas_dir in ['X', 'Y']:
-            row = Series({'Depth': depth,
-                          "Measure Direction": meas_dir,
-                          "Rotation": RZ(np.pi / 2, 0),
-                          "Change of Basis": Program(I(0)),
-                          "Measure Qubits": [0]
-                          })
-            prog = _make_prog_from_df(row)
             idx = ((depth - 1) if meas_dir == 'Y' else depth) % 4
             expected = expected_outcomes[idx]
-            result = np.average(_run_rpe_program(qvm, prog, [[0]], 5000))
-            assert np.allclose(expected, result, atol=.005)
+            exp = [res.expectation for res in result.layers[0].results if
+                   res.setting.out_operator[q] == meas_dir][0]
+            assert np.allclose(expected, exp, atol=.05)
 
 
 def test_noiseless_rpe(qvm):
     qvm.qam.random_seed = 5
     angle = pi / 4 - .5  # pick arbitrary angle
-    expt = rpe.generate_rpe_experiment(RZ(angle, 0), I(0), num_depths=7)
-    expt = rpe.acquire_rpe_data(qvm, expt, multiplicative_factor=10.)
-    xs, ys, x_stds, y_stds = rpe.get_moments(expt)
-    result = rpe.estimate_phase_from_moments(xs, ys, x_stds, y_stds)
-    assert np.abs(angle - result) < 2 * np.sqrt(rpe.get_variance_upper_bound(expt))
-    # test that wrapper yields same result
-    result = rpe.robust_phase_estimate(expt)
-    assert np.abs(angle - result) < 2 * np.sqrt(rpe.get_variance_upper_bound(expt))
+    q = 0
+    expt = rpe.generate_rpe_experiment(RZ(angle, q),
+                                       *rpe.all_eigenvector_prep_meas_settings([q], I(q)),
+                                       num_depths=7)
+    res = rpe.acquire_rpe_data(qvm, [expt], multiplicative_factor=10.)[0]
+    result = rpe.robust_phase_estimate(res)
+    assert np.abs(angle - result) < 2 * np.sqrt(rpe.get_variance_upper_bound(res))
 
 
 def test_noisy_rpe(qvm):
     qvm.qam.random_seed = 5
     angles = pi * np.linspace(2 / 9, 2.0 - 2 / 9, 3)
     add_error = .15
+    q = 0
 
     def add_damping_dephasing_noise(prog, T1, T2, gate_time):
         p = Program()
         p.defgate("noise", np.eye(2))
-        p.define_noisy_gate("noise", [0], damping_after_dephasing(T1, T2, gate_time))
+        p.define_noisy_gate("noise", [q], damping_after_dephasing(T1, T2, gate_time))
         for elem in prog:
             p.inst(elem)
             if isinstance(elem, Measurement):
                 continue  # skip measurement
-            p.inst(("noise", 0))
+            p.inst(("noise", q))
         return p
 
-    def add_noise_to_experiments(df, t1, t2, p00, p11):
+    def add_noise_to_experiments(expt, t1, t2, p00, p11, q):
         gate_time = 200 * 10 ** (-9)
-        df["Program"] = Series([
-            add_damping_dephasing_noise(prog, t1, t2, gate_time).define_noisy_readout(0, p00, p11)
-            for prog in df["Program"].values])
+        for layer in expt.layers:
+            layer.sequence = [
+                add_damping_dephasing_noise(prog, t1, t2, gate_time).define_noisy_readout(q, p00,
+                                                                                          p11)
+                for prog in layer.sequence]
 
     tolerance = .1
     # scan over each angle and check that RPE correctly predicts the angle to within .1 radians
     for angle in angles:
-        RH = Program(RY(-pi / 4, 0)).inst(RZ(angle, 0)).inst(RY(pi / 4, 0))
-        evecs = rpe.bloch_rotation_to_eigenvectors(pi / 4, 0)
-        cob = rpe.get_change_of_basis_from_eigvecs(evecs)
-        expt = rpe.generate_rpe_experiment(RH, cob, num_depths=7)
-        expt = rpe.add_programs_to_rpe_dataframe(qvm, expt)
-        add_noise_to_experiments(expt, 25 * 10 ** (-6.), 20 * 10 ** (-6.), .92, .87)
-        expt = rpe.acquire_rpe_data(qvm, expt, multiplicative_factor=5., additive_error=add_error)
+        RH = Program(RY(-pi / 4, q)).inst(RZ(angle, q)).inst(RY(pi / 4, q))
+        evecs = rpe.bloch_rotation_to_eigenvectors(pi / 4, q)
+        cob_matrix = rpe.get_change_of_basis_from_eigvecs(evecs)
+        cob = rpe.change_of_basis_matrix_to_quil(qvm, [q], cob_matrix)
+        prep, meas, settings = rpe.all_eigenvector_prep_meas_settings([q], cob)
+        expt = rpe.generate_rpe_experiment(RH, prep, meas, settings, num_depths=7)
+        add_noise_to_experiments(expt, 25 * 10 ** (-6.), 20 * 10 ** (-6.), .92, .87, q)
+        expt = rpe.acquire_rpe_data(qvm, [expt], multiplicative_factor=5.,
+                                    additive_error=add_error)[0]
         phase_estimate = rpe.robust_phase_estimate(expt)
         assert np.allclose(phase_estimate, angle, atol=tolerance)
