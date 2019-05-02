a5c6d4e7b4b7bfe73a8dd40d251402c9381042dd
Josh Combes <joshcombes@users.noreply.github.com>
2019-05-03 06:14:19 +1000

    use dim in docstrings and code instead of d or D (Issue #92) (#112)
    * purity code and docstring:  D --> dim
    
    * d to dim
    + some other improvements
    
    * dim2 to dim_squared
    
    * d1 and d2 to dim1, dim2
    
    * ^ to  **
    
    * dim2 to dim_squared

--- forest/benchmarking/distance_measures.py
+++ forest/benchmarking/distance_measures.py
@@ -12,19 +12,25 @@ from scipy.optimize import minimize_scalar
 
 def purity(rho, dim_renorm=False):
     """
-    Calculates the purity P of a quantum state.
+    Calculates the purity P = tr[ρ**2] of a quantum state ρ.
 
-    If the dimensional renormalization flag is FALSE (default) then  1/D ≤ P ≤ 1.
+    As stated above lower value of the purity depends on the dimension of ρ's Hilbert space. For
+    some applications this can be undesirable. For this reason we introduce an optional dimensional
+    renormalization flag with the following behavior
+
+    If the dimensional renormalization flag is FALSE (default) then  1/dim ≤ P ≤ 1.
     If the dimensional renormalization flag is TRUE then 0 ≤ P ≤ 1.
 
-    :param rho: Is a D x D positive matrix.
+    where dim is the dimension of ρ's Hilbert space.
+
+    :param rho: Is a dim by dim positive matrix with unit trace.
     :param dim_renorm: Boolean, default False.
     :return: P the purity of the state.
     """
     if dim_renorm:
-        D = np.shape(rho)[0]
+        dim = np.shape(rho)[0]
         Ptemp = np.trace(np.matmul(rho, rho))
-        P = (D / (D - 1.0)) * (Ptemp - 1.0 / D)
+        P = (dim / (dim - 1.0)) * (Ptemp - 1.0 / dim)
     else:
         P = np.trace(np.matmul(rho, rho))
     return P
@@ -38,8 +44,9 @@ def fidelity(rho, sigma):
 
     The fidelity obeys 0 ≤ F(rho,sigma) ≤ 1, where F(rho,sigma)=1 iff rho = sigma and
     F(rho,sigma)= 0 iff
-    :param rho: Is a D x D positive matrix.
-    :param sigma: Is a D x D positive matrix.
+
+    :param rho: Is a dim by dim positive matrix with unit trace.
+    :param sigma: Is a dim by dim positive matrix with unit trace.
     :return: Fidelity which is a scalar.
     """
     return (np.trace(sqrtm(np.matmul(np.matmul(sqrtm(rho), sigma), sqrtm(rho))))) ** 2
@@ -50,8 +57,8 @@ def trace_distance(rho, sigma):
     Computes the trace distance between two states rho and sigma i.e.
     T(rho,sigma) = (1/2)||rho-sigma||_1 , where ||X||_1 denotes the 1 norm of X.
 
-    :param rho: Is a D x D positive matrix.
-    :param sigma: Is a D x D positive matrix.
+    :param rho: Is a dim by dim positive matrix with unit trace.
+    :param sigma: Is a dim by dim positive matrix with unit trace.
     :return: Trace distance which is a scalar.
     """
     return (0.5) * np.linalg.norm(rho - sigma, 1)
@@ -62,8 +69,8 @@ def bures_distance(rho, sigma):
     Computes the Bures distance between two states rho and sigma i.e.
     D_B(rho,sigma)^2 = 2(1- sqrt[F(rho,sigma)]) , where F(rho,sigma) is the fidelity.
 
-    :param rho: Is a D x D positive matrix with unit trace.
-    :param sigma: Is a D x D positive matrix with unit trace.
+    :param rho: Is a dim by dim positive matrix with unit trace.
+    :param sigma: Is a dim by dim positive matrix with unit trace.
     :return: Bures distance which is a scalar.
     """
     return np.sqrt(2 * (1 - np.sqrt(fidelity(rho, sigma))))
@@ -75,8 +82,8 @@ def bures_angle(rho, sigma):
     D_A(rho,sigma) = arccos(sqrt[F(rho,sigma)]) , where F(rho,sigma) is the fidelity.
     The Bures angle is a measure of statistical distance between quantum states.
 
-    :param rho: Is a D x D positive matrix.
-    :param sigma: Is a D x D positive matrix.
+    :param rho: Is a dim by dim positive matrix with unit trace.
+    :param sigma: Is a dim by dim positive matrix with unit trace.
     :return: Bures angle which is a scalar.
     """
     return np.arccos(np.sqrt(fidelity(rho, sigma)))
@@ -84,13 +91,28 @@ def bures_angle(rho, sigma):
 
 def quantum_chernoff_bound(rho, sigma):
     """
-    Computes the exponent of the quantum Chernoff bound between rho and sigma.
+    Computes the quantum Chernoff bound between rho and sigma. It is defined as
 
-    :param rho: Is a D x D positive matrix.
-    :param sigma: Is a D x D positive matrix.
-    :return: the exponent of the quantum Chernoff bound angle which is a scalar.
+    ξ_{QCB}(rho,sigma) = - log[ min_{0\le s\le 1} tr(rho**s sigma**{1-s} ].
+
+    It is also common to study the non-logarithmic variety of the quantum Chernoff bound denoted as
+
+    Q_{QCB}(rho,sigma) = min_{0\le s\le 1} tr(rho**s sigma**{1-s}.
+
+    The quantum Chernoff bound has many nice properties, see [QCB]. Importantly it is
+    operationally important in the following context. Given n copies of rho or sigma the minimum
+    error probability for discriminating for rho from sigma is P_{e,min,n} ~ exp[-n ξ_{QCB}].
+
+    [QCB] The Quantum Chernoff Bound
+          Audenaert et al.,
+          Phys. Rev. Lett. 98, 160501 (2007)
+          https://dx.doi.org/10.1103/PhysRevLett.98.160501
+          https://arxiv.org/abs/quant-ph/0610027
+
+    :param rho: Is a dim by dim positive matrix with unit trace.
+    :param sigma: Is a dim by dim positive matrix with unit trace.
+    :return: the non-logarithmic quantum Chernoff bound and the s achieving the minimum.
     """
-
     def f(s):
         s = np.real_if_close(s)
         return np.trace(
@@ -108,8 +130,8 @@ def hilbert_schmidt_ip(A, B):
         HS = (A|B) = Tr[A^\dagger B]
     where |B) = vec(B) and (A| is the dual vector to |A).
 
-    :param A: Is a D x D matrix.
-    :param B: Is a D x D matrix.
+    :param A: Is a dim by dim positive matrix with unit trace.
+    :param B: Is a dim by dim positive matrix with unit trace.
     :return: HS inner product which is a scalar.
     """
     return np.trace(np.matmul(np.transpose(np.conj(A)), B))
@@ -124,8 +146,8 @@ def smith_fidelity(rho, sigma, power):
     At present there is no known operational interpretation of the Smith fidelity for an arbitrary
     power.
 
-    :param rho: Is a D x D positive matrix.
-    :param sigma: Is a D x D positive matrix.
+    :param rho: Is a dim by dim positive matrix with unit trace.
+    :param sigma: Is a dim by dim positive matrix with unit trace.
     :param power: Is a positive scalar less than 2.
     :return: Smith Fidelity which is a scalar.
     """
@@ -147,8 +169,8 @@ def total_variation_distance(P, Q):
     where tvd(P,Q) is in [0,1]. There is an alternate definition for non-finite alphabet measures
     involving a supremum.
 
-    :param P: Is a numpy array of length D.
-    :param Q: Is a numpy array of length D.
+    :param P: Is a numpy array of length dim.
+    :param Q: Is a numpy array of length dim.
     :return: total variation distance which is a scalar.
     """
     if len(P) != len(Q):
@@ -166,13 +188,14 @@ def process_fidelity(pauli_lio0: np.ndarray, pauli_lio1: np.ndarray) -> float:
 
     The expression is
 
-             F_process(E,F) = ( Tr[E^\dagger F] + d ) / (d^2 + d),
+             F_process(E,F) = ( Tr[E^\dagger F] + dim ) / (dim^2 + dim),
 
     which is sometimes written as
 
-            F_process(E,F) = ( d F_e + 1 ) / (d + 1)
+            F_process(E,F) = ( dim F_e + 1 ) / (dim + 1)
 
-    where F_e is the entanglement fidelity see https://arxiv.org/abs/quant-ph/9807091 .
+    where dim is the dimension of the Hilbert space asociated with E and F, and F_e is the
+    entanglement fidelity see https://arxiv.org/abs/quant-ph/9807091 .
 
     NOTE: F_process is sometimes "gate fidelity" and F_e is sometimes called "process fidelity".
 
@@ -188,14 +211,14 @@ def process_fidelity(pauli_lio0: np.ndarray, pauli_lio1: np.ndarray) -> float:
            https://doi.org/10.1016/S0375-9601(02)01272-0
            https://arxiv.org/abs/quant-ph/0205035
 
-    :param pauli_lio0: A D^2 x D^2 pauli-liouville matrix (where D is the Hilbert space dimension)
-    :param pauli_lio1: A D^2 x D^2 pauli-liouville matrix (where D is the Hilbert space dimension)
+    :param pauli_lio0: A dim**2 by dim**2 Pauli-Liouville matrix
+    :param pauli_lio1: A dim**2 by dim**2 Pauli-Liouville matrix
     :return: The process fidelity between pauli_lio0 and pauli_lio1 which is a scalar.
     """
     assert pauli_lio0.shape == pauli_lio1.shape
     assert pauli_lio0.shape[0] == pauli_lio1.shape[1]
-    dim2 = pauli_lio0.shape[0]
-    dim = int(np.sqrt(dim2))
+    dim_squared = pauli_lio0.shape[0]
+    dim = int(np.sqrt(dim_squared))
 
     Fe = np.trace(np.matmul(np.transpose(np.conj(pauli_lio0)), pauli_lio1)) / (dim ** 2)
 
@@ -217,8 +240,8 @@ def diamond_norm_distance(choi0: np.ndarray, choi1: np.ndarray) -> float:
 
     This calculation becomes very slow for 4 or more qubits.
 
-    :param choi0: A 4^N x 4^N matrix (where N is the number of qubits)
-    :param choi1: A 4^N x 4^N matrix (where N is the number of qubits)
+    :param choi0: A 4**N by 4**N matrix (where N is the number of qubits)
+    :param choi1: A 4**N by 4**N matrix (where N is the number of qubits)
 
     """
     # Kudos: Based on MatLab code written by Marcus P. da Silva
@@ -226,8 +249,8 @@ def diamond_norm_distance(choi0: np.ndarray, choi1: np.ndarray) -> float:
     import cvxpy as cvx
     assert choi0.shape == choi1.shape
     assert choi0.shape[0] == choi1.shape[1]
-    dim2 = choi0.shape[0]
-    dim = int(np.sqrt(dim2))
+    dim_squared = choi0.shape[0]
+    dim = int(np.sqrt(dim_squared))
 
     delta_choi = choi0 - choi1
     delta_choi = (delta_choi.conj().T + delta_choi) / 2  # Enforce Hermiticity
@@ -239,13 +262,13 @@ def diamond_norm_distance(choi0: np.ndarray, choi1: np.ndarray) -> float:
     constraints += [cvx.trace(rho) == 1]
 
     # W must be Hermitian, positive semidefinite
-    W = cvx.Variable([dim2, dim2], complex=True)
+    W = cvx.Variable([dim_squared, dim_squared], complex=True)
     constraints += [W == W.H]
     constraints += [W >> 0]
 
     constraints += [(W - cvx.kron(np.eye(dim), rho)) << 0]
 
-    J = cvx.Parameter([dim2, dim2], complex=True)
+    J = cvx.Parameter([dim_squared, dim_squared], complex=True)
     objective = cvx.Maximize(cvx.real(cvx.trace(J.H * W)))
 
     prob = cvx.Problem(objective, constraints)
@@ -271,7 +294,7 @@ def watrous_bounds(choi: np.ndarray) -> float:
     StackOverflow answer, although the results can also be found scattered in 
     the literature.
 
-    :param choi: d1 x d2 matrix (for qubits, di = 4^Ni, where Ni is a number of qubits)
+    :param choi: dim1 by dim2 matrix (for qubits, dimi = 4**Ni, where Ni is a number of qubits)
     """
 
     if len(choi.shape) != 2:
