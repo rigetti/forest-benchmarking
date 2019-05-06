6bb3584a12583516703e541005fa700eeaf7e74b
Joshua Combes <joshua@rigetti.com>
2019-05-06 13:04:18 +1000

    edits

--- forest/benchmarking/plotting/process_and_state.py
+++ forest/benchmarking/plotting/process_and_state.py
@@ -1,47 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def plot_pauli_rep_of_state(state_pl_basis, ax, labels, title):
     """
     Visualize a quantum state in the Pauli-Liouville basis.
-
-    Example:
-
-    from forest.benchmarking.superoperator_tools import *
-    from forest.benchmarking.utils import n_qubit_pauli_basis
-
-    # zero state in the (Z) computational basis
-    rho_std_basis = np.array([[1, 0], [0, 0]])
-
-    # change to Pauli-Liouville basis
-    n_qubits = 1
-    pl_basis = n_qubit_pauli_basis(n_qubits)
-    c2p = computational2pauli_basis_matrix(2*n_qubits)
-    rho_pl_basis = np.real(c2p@vec(rho_std_basis))
-
-    #plot
-    fig, ax = plt.subplots(1)
-    plot_pauli_rep_of_state(rho_pl_basis, ax, pl_basis.labels, 'Zero state |0>')
-
 
     :param numpy.ndarray state_pl_basis: The quantum state represented in the Pauli-Liouville basis.
     :param ax: The matplotlib axes.
     :param labels: The labels for the operator basis states.
     :param title: The title for the plot.
+
+    Examples
+    --------
+    from forest.benchmarking.superoperator_tools import *
+    from forest.benchmarking.utils import n_qubit_pauli_basis
+    # zero state in the (Z) computational basis
+    rho_std_basis = np.array([[1, 0], [0, 0]])
+    # change to Pauli-Liouville basis
+    n_qubits = 1
+    pl_basis = n_qubit_pauli_basis(n_qubits)
+    c2p = computational2pauli_basis_matrix(2*n_qubits)
+    rho_pl_basis = np.real(c2p@vec(rho_std_basis))
+    # plot
+    fig, ax = plt.subplots(1)
+    plot_pauli_rep_of_state(rho_pl_basis, ax, pl_basis.labels, 'Zero state |0>')
     """
-    if len(state_pl_basis.shape)==1:
+    if len(state_pl_basis.shape) == 1:
         raise ValueError("You must pass in a (N by 1) or a (1 by N) numpy.ndarray")
-    if np.iscomplexobj(rho_std_basis):
+    if np.iscomplexobj(state_pl_basis):
         raise ValueError("You must pass in a real vector")
 
-    im = ax.imshow(state_pl_basis, interpolation="nearest", cmap="RdBu", vmin=-1/2, vmax=1/2)
+    im = ax.imshow(state_pl_basis, interpolation="nearest", cmap="RdBu", vmin=-1 / 2, vmax=1 / 2)
     dim = len(labels)
     # make the colorbar ticks look pretty
     rows, cols = state_pl_basis.shape
-    if rows>cols:
-        cb = plt.colorbar(im, ax=ax,ticks=[-1/2,-1/4, 0, 1/4, 1/2])
+    if rows > cols:
+        cb = plt.colorbar(im, ax=ax, ticks=[-1 / 2, -1 / 4, 0, 1 / 4, 1 / 2])
         ticklabs = cb.ax.get_yticklabels()
-        cb.ax.set_yticklabels(ticklabs,ha='right')
+        cb.ax.set_yticklabels(ticklabs, ha='right')
         cb.ax.yaxis.set_tick_params(pad=35)
         # axis labels etc
         ax.set_xlabel("Coefficient")
@@ -50,11 +47,39 @@ def plot_pauli_rep_of_state(state_pl_basis, ax, labels, title):
         ax.set_ylabel("Pauli Operator")
         ax.set_yticklabels(labels)
     else:
-        cb = plt.colorbar(im, ax=ax,ticks=[-1/2,-1/4, 0, 1/4, 1/2],orientation="horizontal", pad=0.23)
+        cb = plt.colorbar(im, ax=ax, ticks=[-1 / 2, -1 / 4, 0, 1 / 4, 1 / 2],
+                          orientation="horizontal",
+                          pad=0.22)
         ax.set_ylabel("Coefficient")
         ax.set_yticks([])
         ax.set_xticks(range(dim))
         ax.set_xlabel("Pauli Operator")
         ax.set_xticklabels(labels)
     ax.set_title(title)
+    ax.grid(False)
+
+
+def plot_pauli_transfer_matrix(ptransfermatrix, ax, labels, title):
+    """
+    Visualize a quantum process using the Pauli-Liouville representation (aka the Pauli Transfer
+    Matrix) of the process.
+
+    :param numpy.ndarray ptransfermatrix: The Pauli Transfer Matrix
+    :param ax: The matplotlib axes.
+    :param labels: The labels for the operator basis states.
+    :param title: The title for the plot
+    """
+    im = ax.imshow(ptransfermatrix, interpolation="nearest", cmap="RdBu", vmin=-1, vmax=1)
+    dim = len(labels)
+    cb = plt.colorbar(im, ax=ax, ticks=[-1, -3 / 4, -1 / 2, -1 / 4, 0, 1 / 4, 1 / 2, 3 / 4, 1])
+    ticklabs = cb.ax.get_yticklabels()
+    cb.ax.set_yticklabels(ticklabs, ha='right')
+    cb.ax.yaxis.set_tick_params(pad=35)
+    ax.set_xticks(range(dim))
+    ax.set_xlabel("Input Pauli Operator")
+    ax.set_yticks(range(dim))
+    ax.set_ylabel("Output Pauli Operator")
+    ax.set_title(title)
+    ax.set_xticklabels(labels, rotation=45)
+    ax.set_yticklabels(labels)
     ax.grid(False)
