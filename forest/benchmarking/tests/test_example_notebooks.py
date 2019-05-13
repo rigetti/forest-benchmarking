b97c7d1800fc37d9dd1ac4303e6125df7dfc8620
Kyle <kyle.gulshen@rigetti.com>
2019-05-13 10:27:51 -0400

    Corrected path to examples.

--- forest/benchmarking/tests/test_example_notebooks.py
+++ forest/benchmarking/tests/test_example_notebooks.py
@@ -2,12 +2,14 @@ import os
 import pytest
 from subprocess import PIPE, run
 
-EXAMPLES_PATH = '../../../examples/'
+EXAMPLES_PATH = '../../examples/'
+
 
 # from https://stackoverflow.com/a/36058256
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
     return result.stdout
+
 
 # change directory to
 os.chdir(EXAMPLES_PATH)
@@ -19,10 +21,11 @@ os.chdir(EXAMPLES_PATH)
 # ~ 6 min; passed 2019/05/13
 @pytest.mark.slow
 def test_direct_fidelity_estimation_nb():
-    #print(out('ls'))
+    # print(out('ls'))
     output_str = out('pytest --nbval-lax direct_fidelity_estimation.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~5 sec
 def test_distance_measures_nb():
@@ -30,17 +33,20 @@ def test_distance_measures_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~5 sec
 def test_direct_entangled_states_nb():
     output_str = out('pytest --nbval-lax entangled_states.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~5 sec
 def test_Hinton_Plots_nb():
     output_str = out('pytest --nbval-lax Hinton-Plots.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~17 min; passed 2019/05/13
 @pytest.mark.slow
@@ -49,11 +55,13 @@ def test_quantum_volume_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~5 sec
 def test_qubit_spectroscopy_cz_ramsey_nb():
     output_str = out('pytest --nbval-lax qubit_spectroscopy_cz_ramsey.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~5 sec
 def test_qubit_spectroscopy_rabi_nb():
@@ -61,11 +69,13 @@ def test_qubit_spectroscopy_rabi_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~5 sec
 def test_qubit_spectroscopy_t1_nb():
     output_str = out('pytest --nbval-lax qubit_spectroscopy_t1.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~5 sec
 def test_qubit_spectroscopy_t2_nb():
@@ -73,11 +83,13 @@ def test_qubit_spectroscopy_t2_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~5 sec
 def test_random_operators_nb():
     output_str = out('pytest --nbval-lax random_operators.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~5 min; passed 2019/05/13
 @pytest.mark.slow
@@ -86,11 +98,13 @@ def test_randomized_benchmarking_interleaved_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~14 sec
 def test_randomized_benchmarking_simultaneous_nb():
     output_str = out('pytest --nbval-lax randomized_benchmarking_simultaneous.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~7 min; passed 2019/05/13
 @pytest.mark.slow
@@ -99,17 +113,20 @@ def test_randomized_benchmarking_unitarity_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~3 sec
 def test_readout_fidelity_nb():
     output_str = out('pytest --nbval-lax readout_fidelity.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~4 sec
 def test_ripple_adder_benchmark_nb():
     output_str = out('pytest --nbval-lax ripple_adder_benchmark.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # ~40 sec; passed 2019/05/13
 @pytest.mark.slow
@@ -118,17 +135,20 @@ def test_robust_phase_estimation_nb():
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~10 sec
 def test_state_and_process_plots_nb():
     output_str = out('pytest --nbval-lax state_and_process_plots.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
 
+
 # ~4 sec
 def test_superoperator_tools_nb():
     output_str = out('pytest --nbval-lax superoperator_tools.ipynb')
     print(output_str)
     assert output_str.find('failed') is -1
+
 
 # Notebook fails at the moment
 # def test_tomography_process_nb():
