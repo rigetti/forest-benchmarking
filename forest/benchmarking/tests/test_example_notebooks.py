3fc30f13b6514fda236803f669d4499f3e64524b
Joshua Combes <joshua@rigetti.com>
2019-05-14 09:48:46 +1000

    added some helper functions to check if the output from nbval was an empty string as per Peter's suggestion.

--- forest/benchmarking/tests/test_example_notebooks.py
+++ forest/benchmarking/tests/test_example_notebooks.py
@@ -11,8 +11,25 @@ def out(command):
     return result.stdout
 
 
-def run_notebook(filename):
+def run_notebook(filename: str):
     return out('pytest --nbval-lax ' + EXAMPLES_PATH + f'/{filename}')
+
+
+def _is_not_empty_str(string: str) -> bool:
+    # bool(string) tests if the string is empty
+    # bool(string.strip()) tests if the string is full of spaces
+    return bool(string and string.strip())
+
+
+def check_for_failure_and_empty_output(output: str):
+    '''
+    Searches the string for the word 'failed' and checks if the string is empty of full of spaces.
+    If any of those conditions are met False is returned.
+
+    :param output: stdout from 'pytest --nbval-lax notebook.ipynb'
+    :return: bool
+    '''
+    return (output.find('failed') is -1) and _is_not_empty_str(output)
 
 
 # NOTES:
@@ -20,9 +37,9 @@ def run_notebook(filename):
 #
 # >pytest --nbval-lax some_notebook.ipynb
 #
-# The --nbval flag runs the IPython Notebook Validation plugin which will collect and test
-# notebook cells, comparing their outputs with those saved in the file.
-# By adding the optional -lax flag we fail if there is an error in executing a cell.
+# The --nbval flag runs the notebook validation plugin (to collect and test notebook cells,
+# comparing their outputs with those saved in the file). By adding the optional -lax flag we fail
+# if there is an error in executing a cell.
 #
 # (2) We are skipping any test that is longer than 30 seconds.
 #
@@ -39,28 +56,28 @@ def test_direct_fidelity_estimation_nb():
     # print(out('ls'))
     output_str = run_notebook('direct_fidelity_estimation.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_distance_measures_nb():
     output_str = run_notebook('distance_measures.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_direct_entangled_states_nb():
     output_str = run_notebook('entangled_states.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_Hinton_Plots_nb():
     output_str = run_notebook('Hinton-Plots.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~17 min; passed 2019/05/13
@@ -68,42 +85,42 @@ def test_Hinton_Plots_nb():
 def test_quantum_volume_nb():
     output_str = run_notebook('quantum_volume.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_qubit_spectroscopy_cz_ramsey_nb():
     output_str = run_notebook('qubit_spectroscopy_cz_ramsey.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_qubit_spectroscopy_rabi_nb():
     output_str = run_notebook('qubit_spectroscopy_rabi.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_qubit_spectroscopy_t1_nb():
     output_str = run_notebook('qubit_spectroscopy_t1.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_qubit_spectroscopy_t2_nb():
     output_str = run_notebook('qubit_spectroscopy_t2.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 sec
 def test_random_operators_nb():
     output_str = run_notebook('random_operators.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~5 min; passed 2019/05/13
@@ -111,14 +128,14 @@ def test_random_operators_nb():
 def test_randomized_benchmarking_interleaved_nb():
     output_str = run_notebook('randomized_benchmarking_interleaved.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~14 sec
 def test_randomized_benchmarking_simultaneous_nb():
     output_str = run_notebook('randomized_benchmarking_simultaneous.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~7 min; passed 2019/05/13
@@ -126,21 +143,21 @@ def test_randomized_benchmarking_simultaneous_nb():
 def test_randomized_benchmarking_unitarity_nb():
     output_str = run_notebook('randomized_benchmarking_unitarity.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~3 sec
 def test_readout_fidelity_nb():
     output_str = run_notebook('readout_fidelity.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~4 sec
 def test_ripple_adder_benchmark_nb():
     output_str = run_notebook('ripple_adder_benchmark.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~40 sec; passed 2019/05/13
@@ -148,21 +165,21 @@ def test_ripple_adder_benchmark_nb():
 def test_robust_phase_estimation_nb():
     output_str = run_notebook('robust_phase_estimation.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~10 sec
 def test_state_and_process_plots_nb():
     output_str = run_notebook('state_and_process_plots.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # ~4 sec
 def test_superoperator_tools_nb():
     output_str = run_notebook('superoperator_tools.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
 
 
 # Notebook fails at the moment
@@ -176,4 +193,4 @@ def test_superoperator_tools_nb():
 def test_tomography_state_nb():
     output_str = run_notebook('tomography_state.ipynb')
     print(output_str)
-    assert output_str.find('failed') is -1
+    assert check_for_failure_and_empty_output(output_str)
