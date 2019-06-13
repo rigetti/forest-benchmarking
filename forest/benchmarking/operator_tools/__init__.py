047a9db6b24349145ff8da675d14e643a2f051f1
Joshua Combes <joshua@rigetti.com>
2019-06-13 14:58:25 +1000

    update

--- forest/benchmarking/operator_tools/__init__.py
+++ forest/benchmarking/operator_tools/__init__.py
@@ -1,5 +1,6 @@
 from .apply_superoperator import *
 from .channel_approximation import *
+from .compose_superoperators import *
 from .project_superoperators import *
 from .superoperator_transformations import *
 from .random_operators import *
