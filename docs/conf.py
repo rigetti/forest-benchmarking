7b60485740331f254fe5bccc69ee4f1cc4f9af84
Joshua Combes <joshua@rigetti.com>
2019-07-02 11:37:58 +1000

    asdf

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,10 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../forest.benchmarking/'))
 
+print(os.getcwd())
 
 # -- Project information -----------------------------------------------------
 
