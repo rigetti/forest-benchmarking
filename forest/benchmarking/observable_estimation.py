cf0a12f26f01835c3ac59f702a916870ce67ccd3
Kyle <kyle.gulshen@rigetti.com>
2019-07-03 16:58:54 -0400

    Finish basics of obs_est nb.

--- forest/benchmarking/observable_estimation.py
+++ forest/benchmarking/observable_estimation.py
@@ -618,6 +618,7 @@ def group_settings(obs_expt: ObservablesExperiment,
     """
     Group settings that are diagonal in a shared tensor product basis (TPB) to minimize number
     of QPU runs.
+
     Background
     ----------
     Given some PauliTerm operator, the 'natural' tensor product basis to
@@ -1116,22 +1117,29 @@ def ratio_variance(a: Union[float, np.ndarray],
                    b: Union[float, np.ndarray],
                    var_b: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
     r"""
-    Given random variables 'A' and 'B', compute the variance on the ratio Y = A/B. Denote the
-    mean of the random variables as a = E[A] and b = E[B] while the variances are var_a = Var[A]
-    and var_b = Var[B] and the covariance as Cov[A,B]. The following expression approximates the
-    variance of Y
+    Given random variables 'A' and 'B', compute the variance on the ratio Y = A/B.
+
+    Denote the mean of the random variables as a = E[A] and b = E[B] while the variances are
+    var_a = Var[A] and var_b = Var[B] and the covariance as Cov[A,B]. The following expression
+    approximates the variance of Y
+
     Var[Y] \approx (a/b) ^2 * ( var_a /a^2 + var_b / b^2 - 2 * Cov[A,B]/(a*b) )
+
     We assume the covariance of A and B is negligible, resting on the assumption that A and B
     are independently measured. The expression above rests on the assumption that B is non-zero,
     an assumption which we expect to hold true in most cases, but makes no such assumptions
     about A. If we allow E[A] = 0, then calculating the expression above via numpy would complain
     about dividing by zero. Instead, we can re-write the above expression as
+
     Var[Y] \approx var_a /b^2 + (a^2 * var_b) / b^4
+
     where we have dropped the covariance term as noted above.
+
     See the following for more details:
       - https://doi.org/10.1002/(SICI)1097-0320(20000401)39:4<300::AID-CYTO8>3.0.CO;2-O
       - http://www.stat.cmu.edu/~hseltman/files/ratio.pdf
       - https://en.wikipedia.org/wiki/Taylor_expansions_for_the_moments_of_functions_of_random_variables
+
     :param a: Mean of 'A', to be used as the numerator in a ratio.
     :param var_a: Variance in 'A'
     :param b: Mean of 'B', to be used as the numerator in a ratio.
